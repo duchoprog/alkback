9bbe63089e355899c5cb8244b11b44a07b38d3b2
Pablo Duchovny <pablo.duchovny@gmail.com>
2021-01-25 18:42:35 -0300

    Created express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require('express')
+
+//Create express server
+const app=express()
+
+
+
+
+
+
+
+
+
+// Listen for petitions
+app.listen (4000, ()=>{
+    console.log("Server running on port 4000")
+})
