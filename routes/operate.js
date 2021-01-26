cf6758ce6447fadb8019948b691591858545437a
Pablo Duchovny <pablo.duchovny@gmail.com>
2021-01-26 12:35:44 -0300

    Added db-connection.js

--- routes/operate.js
+++ routes/operate.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const db= require('../src/db-connection')
 
 router.post('/operate', (req, res)=>{
+    
     res.send ("Hello operate")
+db.query(sql, (err, result)=>{
+    if(err) throw err;
+    res.send()
 })
+
+})
+
+
 
 module.exports = router;
---
+++ src/db-connection.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database:'alkdb'
+    });
+
+  module.exports=db
