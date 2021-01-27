6703f2e76a32a6833155c509cdafb9f6bf522076
Pablo Duchovny <pablo.duchovny@gmail.com>
2021-01-27 09:53:48 -0300

    Added functionality to retrieve.js

--- routes/retrieve.js
+++ routes/retrieve.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db= require('../src/db-connection')
 
 router.get('/retrieve', (req, res)=>{
-    res.send ("Hello retrieve")
+
+    let sql= `SELECT * FROM (
+        SELECT * FROM transactions ORDER BY id DESC LIMIT 10) sub 
+        ORDER BY id ASC;`
+    db.query(sql, (err, results)=>{
+        if(err) throw err;  
+           
+        res.send(results)   
+    })
+
 })
+
 
 module.exports = router;
