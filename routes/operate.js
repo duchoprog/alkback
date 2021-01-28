df0fe15ed62744ffd4f80516f2c334629ae27b9b
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-28 09:24:24 -0300

    Removed console.log from operate.js

--- routes/operate.js
+++ routes/operate.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const db= require('../src/db-connection')
+const db = require('../src/db-connection')
 
 
 router.use(express.json())
-router.post('/operate', (req, res)=>{
+router.post('/operate', (req, res) => {
 
-    let data=(req.body)
-    let sql= "INSERT INTO transactions SET ?"
-    db.query(sql, data, (err, result)=>{
-        if(err) throw err;        
+    let data = (req.body)
+    let sql = "INSERT INTO transactions SET ?"
+    db.query(sql, data, (err, result) => {
+        if (err) throw err;
     })
 
 })
