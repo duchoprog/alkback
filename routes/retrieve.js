a4e92988d3cc78b7c693372fe781ffadbd155793
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-28 14:59:43 -0300

    Added BALANCE calculation and added it to res.send

--- routes/retrieve.js
+++ routes/retrieve.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const db= require('../src/db-connection')
+const db = require('../src/db-connection')
 
-router.get('/retrieve', (req, res)=>{
+const getBalance = (array) => {
+    let balance = 0
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].tipo === "gasto") {
+            balance -= array[i].monto
+        } else {
+            balance += array[i].monto
+        }
+    }
+    return balance
+}
 
-    let sql= `SELECT * FROM (
-        SELECT * FROM transactions ORDER BY id DESC LIMIT 10) sub 
-        ORDER BY id ASC;`
-    db.query(sql, (err, results)=>{
-        if(err) throw err;  
-           
-        res.send(Object.assign({}, results))   
+router.get('/retrieve', (req, res) => {
+
+    let sql = `SELECT * FROM  transactions ORDER BY id DESC`
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        results.unshift({ balance: getBalance(results) })
+        console.log(results)
+        res.send(Object.assign({}, results.slice(0, 11)))
     })
 
 })
