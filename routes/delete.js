150b1bd7ca09bcda692bf343234758b5ed5d1b8c
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-28 19:20:10 -0300

    Added Delete funcionality

--- routes/delete.js
+++ routes/delete.js
@@ -4,14 +4,14 @@ const db = require('../src/db-connection')
 
 
 router.use(express.json())
-router.get('/delete', (req, res) => {
+router.post('/delete', (req, res) => {
 
     let data = (req.body)
 
     let sql = `DELETE FROM transactions WHERE id="${data['id']}"`
     db.query(sql, data, (err, result) => {
         if (err) throw err;
-        res.send("Actualizado")
+        res.send("Borrado")
     })
 
 })
